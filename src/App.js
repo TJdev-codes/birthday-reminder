a457428f70b83aacd0db752860025e04525bc2ad
Coder TJ <83976571+TJdev-codes@users.noreply.github.com>
2021-10-25 14:36:25 -0400

    removed button for when there are zero reminders.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import List from './List';
 
 function App() {
   const [people, setPeople] = useState(data)
+  if (people.length === 0) {
+    return (
+      <main>
+      <section className="container">
+        <h3>
+          {people.length} people have birthdays today!
+        </h3>
+        <p>No Reminders for Today</p>
+      </section>
+      </main>
+    )
+  } else {
   return (
     <main>
       <section className="container">
@@ -16,7 +28,7 @@ function App() {
         </button>
       </section>
     </main>
-  )
+  )}
 }
 
 export default App;
